d908560618b31421b3ffc305d762f4b3b732d634
David Simpson <SimpsonD4@cf.ac.uk>
2017-09-28 13:34:48 +0100

    vagrant_ports.sh: Check for entries in global-status, use mktemp, show vagrant id

--- vagrant_ports.sh
+++ vagrant_ports.sh
@@ -18,5 +18,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-echo "------------"
-vagrant global-status | awk 'length($1) == 7 { print $1 " " $5}' | nl -n ln > /tmp/current_vagrant_vms; awk '{ print $3 }' /tmp/current_vagrant_vms | while read line; do echo "Machine path:    ${line}" && echo "Mac Address from Vagrantfile:" && grep "mac:" ${line}/Vagrantfile && vmindir=`cat ${line}/.vagrant/machines/default/virtualbox/id` && VBoxManage showvminfo ${vmindir} | /usr/bin/egrep -i 'name:|host port' && echo "------------"; done
+vagrant global-status | grep -qi "There are no active Vagrant environments"; active_test=`echo $?`
+
+if [ ${active_test} -eq 1 ]; then
+:
+else
+echo "ERROR: vagrant global-status is empty, exiting!"
+exit 1
+fi
+
+TMPFILE=`mktemp /tmp/vagrant.XXXXXXXXXX` || exit 1
+
+echo "------------------------"
+vagrant global-status | awk 'length($1) == 7 { print $1 " " $5}' | nl -n ln > ${TMPFILE};
+
+awk '{ print $3 }' ${TMPFILE} | while read line; do vagrantid=`cut -c1-7 ${line}/.vagrant/machines/default/virtualbox/index_uuid` && echo "Vagrant ID:      ${vagrantid}" && echo "Machine path:    ${line}" && echo "Mac Address from Vagrantfile:" && grep "mac:" ${line}/Vagrantfile && vmindir=`cat ${line}/.vagrant/machines/default/virtualbox/id` && VBoxManage showvminfo ${vmindir} | /usr/bin/egrep -i 'name:|host port' && echo "------------------------"; done
